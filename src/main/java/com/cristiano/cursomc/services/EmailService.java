9e73b6b8aa67ca3c233d9ce73885c702cba39eb3
Cristiano Tiago Modesto <cristianoufo@gmail.com>
2019-10-04 10:13:56 -0300

    Email html

--- src/main/java/com/cristiano/cursomc/services/EmailService.java
+++ src/main/java/com/cristiano/cursomc/services/EmailService.java
@@ -3,9 +3,16 @@ package com.cristiano.cursomc.services;
 import org.springframework.mail.SimpleMailMessage;
 
 import com.cristiano.cursomc.domain.Pedido;
+import javax.mail.internet.MimeMessage;
 
 public interface EmailService {
-	void sendOrderConfirmationEmail(Pedido obj);
-	void sendMail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationEmail(Pedido obj);
+
+    void sendMail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 
 }
