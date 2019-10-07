72d77683fb9215be410fcbe4da0649a86ec87dda
Cristiano Tiago Modesto <cristianoufo@gmail.com>
2019-10-07 11:26:59 -0300

    Refresh token

---
+++ src/main/java/com/cristiano/cursomc/dto/EmailDTO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cristiano.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+/**
+ *
+ * @author cristiano.modesto
+ */
+
+public class EmailDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    public EmailDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/cristiano/cursomc/resources/AuthResource.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cristiano.cursomc.resources;
+
+import com.cristiano.cursomc.dto.EmailDTO;
+import com.cristiano.cursomc.security.JWTUtil;
+import com.cristiano.cursomc.security.UserSS;
+import com.cristiano.cursomc.services.AuthService;
+import com.cristiano.cursomc.services.UserService;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author cristiano.modesto
+ */
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private AuthService service;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.getAuthenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        response.addHeader("access-control-expose-headers", "Authorization");
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/forgot", method = RequestMethod.POST)
+    public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+        service.sendNewPassword(objDto.getEmail());
+        return ResponseEntity.noContent().build();
+    }
+}
